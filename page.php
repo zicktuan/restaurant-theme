bc46924e85e8444084096adef017fe54c84bfe98
zicktuan <anhtuannguyen094@gmail.com>
2020-09-23 17:11:30 +0700

    update

--- page.php
+++ page.php
@@ -1,7 +1,20 @@
 <?php get_header(); ?>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <?php get_template_part('page', 'content');  ?>
-<?php endwhile;
-endif; ?>
-    <?php get_sidebar();?>
+<div class="restbeef_main_wrapper">
+    <div class="restbeef_container">
+        <div class="restbeef_content_wrapper restbeef_no_sidebar">
+
+            <!-- Content Inner -->
+            <div class="restbeef_content">
+                <div class="restbeef_tiny">
+
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        <?php get_template_part('page', 'content');  ?>
+                    <?php endwhile;
+                    endif; ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php get_footer(); ?>
